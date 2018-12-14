8d1d273d058ac1a97f5f13e8a5ac4e0185b08f97
LiZhengHao <1263212577@qq.com>
2018-12-14 16:01:46 +0800

    build project

---
+++ TT/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace TT
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

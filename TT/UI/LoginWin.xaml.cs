9b2cf0c88740ca118ed23c64875e1b3a3a6471ab
LiZhengHao <1263212577@qq.com>
2018-12-14 19:26:01 +0800

    2018年12月14日

---
+++ TT/UI/LoginWin.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TT.UI
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 登陆按钮单击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            Page1 page=new Page1();
+            
+            NavigationWindow window = new NavigationWindow();
+            window.Source = new Uri("/UI/Page1.xaml", UriKind.Relative);
+            window.Show();
+            
+        }
+
+        /// <summary>
+        /// 注册按钮单击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
